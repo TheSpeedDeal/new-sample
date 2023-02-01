6ae99cb11148ee7e944955e79bfe83d9f04e5421
Nylle <frozennylle@gmail.com>
2023-02-01 08:34:39 +0800

    Deleted some unesesary in the Controller

--- app/Http/Controllers/TimerController.php
+++ app/Http/Controllers/TimerController.php
@@ -2,24 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Mail\InviteUsers;
-use App\Models\Genre;
-use App\Models\Music;
-use App\Models\Room;
-use App\Models\User;
-use App\Models\Subject;
-use App\Models\Goal;
 use App\Models\CustomTimer;
-use App\Models\MeetingInvites;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 use Exception;
-use Twilio\Jwt\AccessToken;
-use Twilio\Jwt\Grants\VideoGrant;
-use Twilio\Rest\Client;
 
 class TimerController extends Controller
 {
